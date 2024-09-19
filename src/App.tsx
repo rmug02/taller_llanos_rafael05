703d8839e9715068b213bcb687f1388d1510a048
Rafael llanos <mgueavra8@cuc.edu.co>
2024-09-19 11:56:43 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import chilena from "./assets/img/chilena.png"
+
+function App() {
+
+  return (
+    <>
+     <div>
+        <img src={chilena} alt="Logo de React" />
+        <h1>CR7</h1>
+        <p>SIUUUUUUU</p>
+     </div>
+    </>
+  )
+}
+
+export default App
